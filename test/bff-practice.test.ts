ad736a4c500cb8d06e73a3091b829f4d1e6cb39c
iammahir2020 <mahiralkamal.mak@gmail.com>
2026-08-19 22:25:46 +0600

    Initial commit

---
+++ test/bff-practice.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BffPractice from '../lib/bff-practice-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bff-practice-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BffPractice.BffPracticeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
